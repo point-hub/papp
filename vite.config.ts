f39da8541e0932084a66f47c6bbfcc8cb7228057
Martien Dermawan Tanama <martiendt@gmail.com>
2024-01-03 08:02:42 +0700

    fix: make vue-router external

--- vite.config.ts
+++ vite.config.ts
@@ -34,13 +34,14 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', 'pinia'],
+      external: ['vue', 'pinia', 'vue-router'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           vue: 'Vue',
-          pinia: 'pinia'
+          pinia: 'pinia',
+          'vue-router': 'vueRouter'
         }
       }
     }
