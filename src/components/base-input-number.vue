39c9cea8c7d5ab27f9189372d00a8c77e62147d5
Martien Dermawan Tanama <martiendt@gmail.com>
2024-09-11 06:27:59 +0700

    fix: add prefix and suffix slot

--- src/components/base-input-number.vue
+++ src/components/base-input-number.vue
@@ -36,12 +36,32 @@ const props = withDefaults(defineProps<Props>(), {
 const inputRef = ref()
 const cleave = ref()
 
+const prefixRef = ref()
+const suffixRef = ref()
+const paddingLeft = ref(0)
+const paddingRight = ref(0)
+
 onMounted(() => {
   cleave.value = new Cleave(inputRef.value, {
     numeral: true,
     numeralThousandsGroupStyle: 'thousand',
     numeralDecimalScale: props.decimalLength
   })
+
+  if (props.border === 'full' || prefixRef.value.clientWidth) {
+    paddingLeft.value = prefixRef.value.clientWidth === 0 ? 10 : prefixRef.value.clientWidth
+  }
+  if (props.border === 'full' || suffixRef.value.clientWidth) {
+    paddingRight.value = suffixRef.value.clientWidth === 0 ? 10 : suffixRef.value.clientWidth
+  }
+  setTimeout(() => {
+    if (props.border === 'full' || prefixRef.value.clientWidth) {
+      paddingLeft.value = prefixRef.value.clientWidth === 0 ? 10 : prefixRef.value.clientWidth
+    }
+    if (props.border === 'full' || suffixRef.value.clientWidth) {
+      paddingRight.value = suffixRef.value.clientWidth === 0 ? 10 : suffixRef.value.clientWidth
+    }
+  }, 1000)
 })
 
 const emit = defineEmits<{
@@ -75,16 +95,32 @@ defineExpose({
       class="form-input"
       :class="{
         'text-right': align === 'right',
-        'border-simple px-0!': border === 'simple',
+        'border-simple': border === 'simple',
         'border-full': border === 'full',
-        'border-none px-0!': border === 'none'
+        'border-none': border === 'none'
       }"
       v-model="value"
       :placeholder="props.placeholder"
       :autofocus="props.autofocus"
       :required="props.required"
       :disabled="props.disabled"
+      :style="{
+        paddingLeft: `${paddingLeft}px`,
+        paddingRight: `${paddingRight}px`
+      }"
     />
+    <div
+      ref="suffixRef"
+      class="absolute right-0 h-full flex items-center justify-center text-slate-400 dark:text-slate-300"
+    >
+      <slot name="suffix"></slot>
+    </div>
+    <div
+      ref="prefixRef"
+      class="absolute h-full flex items-center justify-center text-slate-400 dark:text-slate-300"
+    >
+      <slot name="prefix"></slot>
+    </div>
   </component>
 </template>
 
