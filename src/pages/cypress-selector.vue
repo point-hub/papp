9990128fbb8d18ca4bf82617f3ae3a6ee55d8999
Martien Dermawan Tanama <martiendt@gmail.com>
2025-10-06 14:55:04 +0700

    fix: example app style

--- src/pages/cypress-selector.vue
+++ src/pages/cypress-selector.vue
@@ -46,119 +46,46 @@ const onSubmit = () => {
 
 <template>
   <div class="flex flex-col gap-4">
-    <base-card>
-      <template #header>Cypress Selector</template>
+    <base-card title="Cypress Selector">
 
-      <div class="flex flex-col gap-4 mt-10">
-        <base-input
-          layout="horizontal"
-          label="Input Text"
-          v-model="inputText"
-          border="full"
-          data-testid="input-text"
-        />
+      <div class="flex flex-col gap-2">
+        <base-input layout="horizontal" label="Input Text" v-model="inputText" border="full" data-testid="input-text" />
 
-        <base-textarea
-          layout="horizontal"
-          label="Input Textarea"
-          v-model="inputTextarea"
-          border="full"
-          data-testid="input-textarea"
-        />
+        <base-textarea layout="horizontal" label="Input Textarea" v-model="inputTextarea" border="full"
+          data-testid="input-textarea" />
 
-        <base-input-mask
-          layout="horizontal"
-          label="Input Mask"
-          v-model="inputMask"
-          :options="{ creditCard: true }"
-          placeholder="**** **** **** ****"
-          border="full"
-          data-testid="input-mask"
-        />
+        <base-input-mask layout="horizontal" label="Input Mask" v-model="inputMask" :options="{ creditCard: true }"
+          placeholder="**** **** **** ****" border="full" data-testid="input-mask" />
 
-        <base-input-number
-          layout="horizontal"
-          label="Input Number"
-          v-model="inputNumber"
-          border="full"
-          align="left"
-          data-testid="input-number"
-        />
+        <base-input-number layout="horizontal" label="Input Number" v-model="inputNumber" border="full" align="left"
+          data-testid="input-number" />
 
-        <base-datepicker
-          label="Input Date"
-          layout="horizontal"
-          v-model="inputDate"
-          border="full"
-          data-testid="input-date"
-        />
+        <base-datepicker label="Input Date" layout="horizontal" v-model="inputDate" border="full"
+          data-testid="input-date" />
 
-        <base-choosen
-          label="Choosen"
-          layout="horizontal"
-          title="Example"
-          :options="choosenOptions"
-          v-model:selected="choosenSelected"
-          border="full"
-          data-testid="choosen"
-        />
+        <base-choosen label="Choosen" layout="horizontal" title="Example" :options="choosenOptions"
+          v-model:selected="choosenSelected" border="full" data-testid="choosen" />
 
-        <base-select
-          label="Select"
-          layout="horizontal"
-          v-model="selectSelected"
-          :options="selectOptions"
-          border="full"
-          data-testid="select"
-        />
+        <base-select label="Select" layout="horizontal" v-model="selectSelected" :options="selectOptions" border="full"
+          data-testid="select" />
 
         <base-file-upload label="File Upload" layout="horizontal" data-testid="file-upload" />
 
-        <base-radio
-          label="Radio"
-          layout="horizontal"
-          :options="radioOptions"
-          v-model="radioSelected"
-          data-testid="radio"
-        />
+        <base-radio label="Radio" layout="horizontal" :options="radioOptions" v-model="radioSelected"
+          data-testid="radio" />
 
-        <base-switch
-          label="Switch"
-          layout="horizontal"
-          v-model="inputSwitch"
-          data-testid="switch"
-        />
+        <base-switch label="Switch" layout="horizontal" v-model="inputSwitch" data-testid="switch" />
 
-        <base-checkbox
-          label="Checkbox"
-          layout="horizontal"
-          text="Agree"
-          v-model="inputCheckbox"
-          align="left"
-          data-testid="input-checkbox"
-        />
+        <base-checkbox label="Checkbox" layout="horizontal" text="Agree" v-model="inputCheckbox" align="left"
+          data-testid="input-checkbox" />
 
-        <base-range
-          label="Range"
-          layout="horizontal"
-          v-model="inputRange"
-          data-testid="input-range"
-          showText
-        />
-
-        <div>
-          <base-button
-            data-testid="submit-button"
-            @click="onSubmit"
-            color="primary"
-            variant="filled"
-            size="sm"
-          >
-            Submit
-          </base-button>
-        </div>
+        <base-range label="Range" layout="horizontal" v-model="inputRange" data-testid="input-range" showText />
       </div>
     </base-card>
+
+    <base-button data-testid="submit-button" @click="onSubmit" color="primary" variant="filled" size="sm" block>
+      Submit
+    </base-button>
   </div>
 </template>
 
